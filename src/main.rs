d38d97af6e5966d0bdd5eaf39cf56a416c8bbab4
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-02 06:10:23 -0400

    for loop using range

--- src/main.rs
+++ src/main.rs
@@ -25,4 +25,17 @@ fn main() {
     }
 
     println!("LIFTOFF!!!");
+
+//  for loop
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element); 
+    }
+
+//  for loop using "Range"
+    for number in (1..4).rev()  {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
