4b95da0e886bfb239148b9572426a3ad7cc8b5e8
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-02 05:58:12 -0400

    while loop

--- src/main.rs
+++ src/main.rs
@@ -14,4 +14,15 @@ fn main() {
     };
 
     println!("The result is {}", result);
+
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
 }
