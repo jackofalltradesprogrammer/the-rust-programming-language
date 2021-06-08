b9f977afaf8b7eb9abf98341c0de64c2a5befd43
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-08 05:41:02 -0400

    fix the typo

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let s1 = String::from("hello"); // :: is used to namespace that particular function
     let s2 = s1;
 
-    s.push_str(", world!"); 
+    s1.push_str(", world!"); 
     // push_str() appends a literal to a String
 
     // compile error as s1 is no longer valid
