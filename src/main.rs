0398f92afd72c76ca402e37cbbd523e488f2d5e5
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-08 05:39:30 -0400

    literals vs String - stack vs heap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello"); // :: is used to namespace that particular function
+    let s2 = s1;
+
+    s.push_str(", world!"); 
+    // push_str() appends a literal to a String
+
+    // compile error as s1 is no longer valid
+    println!("{}, world!", s1); 
 }
