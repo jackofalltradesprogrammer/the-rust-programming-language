33356d1a9df5c1234e33966e8d96a2ef4351a1cf
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-08 05:47:22 -0400

    clone heap data in a String using clone()

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let s1 = String::from("hello"); // :: is used to namespace that particular function
-    let s2 = s1;
+    let mut s1 = String::from("hello"); // :: is used to namespace that particular function
+   // let s2 = s1;
 
     s1.push_str(", world!"); 
     // push_str() appends a literal to a String
 
     // compile error as s1 is no longer valid
-    println!("{}, world!", s1); 
+    // println!("{}, world!", s1); 
+
+    // use clone function
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
