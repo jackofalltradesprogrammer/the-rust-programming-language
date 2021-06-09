f033b333c834a5a412010cc3eefb7ead78bd3172
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-09 06:54:02 -0400

    refences for var* are not mutable

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,18 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
+
+    // change(&s1);
+    // `some_string` is a `&` reference, so the data it refers to cannot be borrowed as mutable.
 }
 
 fn calculate_length(s: &String) -> usize { // s is a reference to a String
     s.len()
 } // Here, s goes out of scope. But because it does not have ownership of what
   // it refers to, nothing happens.
+
+// References are not mutable for regular varaibles* & generates an error
+// fn change(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
