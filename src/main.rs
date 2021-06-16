13d821e2d5e1f19bcbc258c601bfcae793927a71
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-16 06:06:25 -0400

    return index of the first word

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// write a function that takes a string and returns the first word it finds in that string. 
+// If the function doesn’t find a space in the string, the whole string must be one word, 
+// so the entire string should be returned.
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+    let word = first_word(&s); // word will get the value 5
+    s.clear(); // this empties the String, making it equal to ""
+
+    // word still has the value 5 here, but there's no more string that
+    // we could meaningfully use the value 6 with, word is now totally invalid
+    // The code compiles without any errors but imagine if we have starting and ending indecies both
+    println!("The value of word is {}", word);
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
 }
