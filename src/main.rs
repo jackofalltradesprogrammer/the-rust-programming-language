a5d3dcce6859d504f453e55d1ea84589af67f12f
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-24 06:13:53 -0400

    using single variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // Using single varialbes to calculate area
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+}
+
+// Using single variables to calculate area
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
