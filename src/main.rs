b573f08f4bcdf1c66f7ee02cef9898516b36fa2e
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-24 06:21:49 -0400

    calculate area using tuples

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,22 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
     );
+
+    // Using tuple to group width and height
+    let rect1 = (30, 50); 
+    
+    println!(
+        "The area of the rectangle using TUPLES is {} square pixels.",
+        area_using_tuples(rect1)
+    );
 }
 
 // Using single variables to calculate area
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+// Using tuple to calculate area
+fn area_using_tuples(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
