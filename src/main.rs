2b47d49c60d722f5f65912ce6d3a1cfc97da70fd
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-06-29 06:13:11 -0400

    refactor using struts: more meaning

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,21 @@ fn main() {
         "The area of the rectangle using TUPLES is {} square pixels.",
         area_using_tuples(rect1)
     );
+
+    // Refactoring with Structs: Adding more meaning
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", area_using_struts(&rect1));
 }
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 
 // Using single variables to calculate area
 fn area(width: u32, height: u32) -> u32 {
@@ -25,4 +39,9 @@ fn area(width: u32, height: u32) -> u32 {
 // Using tuple to calculate area
 fn area_using_tuples(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+// Using struts to calulate area
+fn area_using_struts(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
