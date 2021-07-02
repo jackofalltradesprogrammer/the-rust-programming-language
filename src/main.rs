8286ef15011f93764cc25812d64187636ac2cb56
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2021-07-02 05:49:19 -0400

    define method on a struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// function defined on the Rectangle struct
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 }
